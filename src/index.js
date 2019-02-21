07185dc2038fe499a26eeebf853ec47e231df94d
juliastetskaya <juliastetskaya@gmail.com>
2019-02-21 07:47:03 +0300

    Ready!

--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  const result = preferences.reduce((acc, first, index) => {
+    second = preferences[first - 1] - 1;
+    third = preferences[second] - 1;
+    return ((third === index) ? acc + 1 : acc);
+  }, 0);
+  return Math.floor(result / 3);
 };
